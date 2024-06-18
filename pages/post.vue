27268c204d9b15ed76602e3046ced229ce5d741b
soundraven <kin9709@gmail.com>
2024-06-18 17:20:04 +0900

    글 상세보기 페이지 UCard 적용중

--- pages/post.vue
+++ pages/post.vue
@@ -1,4 +1,40 @@
 <template>
-    <div>게시글 상세보기 페이지 입니다.</div>
+    <UContainer>
+        <UCard
+            :ui="{
+                // base: '',
+                // background: 'bg-white dark:bg-gray-900',
+                // divide: 'divide-y divide-gray-200 dark:divide-gray-800',
+                // ring: 'ring-1 ring-gray-200 dark:ring-gray-800',
+                // rounded: 'rounded-lg',
+                // shadow: 'shadow',
+                header: {
+                    base: '',
+                    background: '',
+                    padding: 'px-4 py-5 sm:p-6',
+                },
+                default: {
+                    base: '',
+                    background: '',
+                    padding: 'px-4 py-5 sm:px-6',
+                },
+                footer: {
+                    base: '',
+                    background: '',
+                    padding: 'px-4 py-4 sm:px-6',
+                },
+            }"
+        >
+            <template #header>
+                <div>헤더 영역입니다</div>
+            </template>
+            <template #default>
+                <div class="bg-blue-500">글 내용입니다.</div>
+            </template>
+            <template #footer>
+                <div>푸터 영역입니다</div>
+            </template>
+        </UCard>
+    </UContainer>
 </template>
-<script></script>
+<script setup lang="ts"></script>
