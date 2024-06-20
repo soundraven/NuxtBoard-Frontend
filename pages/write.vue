6bdd61f8a1c7dcad645d0aa2fff7818bf2b3d5a0
soundraven <kin9709@gmail.com>
2024-06-20 16:28:22 +0900

    글작성페이지 틀 완성

--- pages/write.vue
+++ pages/write.vue
@@ -8,19 +8,24 @@
                 placeholder="글 내용을 작성해 주세요"
             />
         </template>
-
+        <UTextarea
+            v-model="content"
+            resize
+            autoresize
+            :maxrows="20"
+            class="min-h-30 max-h-400px"
+            placeholder="글 내용을 작성해 주세요"
+        />
         <template #footer>
-            <UTextarea
-                v-model="content"
-                resize
-                autoresize
-                class="min-h-80"
-                placeholder="글 내용을 작성해 주세요"
-            />
+            <UButton @click="onSubmit">작성</UButton>
         </template>
     </UCard>
 </template>
 <script setup lang="ts">
 const title: Ref<string> = ref("")
 const content: Ref<string> = ref("")
+
+const onSubmit = () => {
+    console.log(title.value, content.value)
+}
 </script>
