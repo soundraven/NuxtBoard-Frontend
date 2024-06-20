68cb3baea89e81216dc7f6d2c4a920be4f714709
soundraven <kin9709@gmail.com>
2024-06-20 16:29:46 +0900

    글수정페이지 틀 완성

---
+++ pages/edit.vue
@@ -0,0 +1,33 @@
+<template>
+    <UCard>
+        <template #header>
+            <UTextarea
+                v-model="title"
+                :rows="1"
+                size="xl"
+                placeholder="글 내용을 작성해 주세요"
+            />
+        </template>
+        <UTextarea
+            v-model="content"
+            resize
+            autoresize
+            :maxrows="20"
+            class="min-h-30 max-h-400px"
+            placeholder="글 내용을 작성해 주세요"
+        />
+        <template #footer>
+            <UButton @click="onSubmit">작성</UButton>
+        </template>
+    </UCard>
+</template>
+<script setup lang="ts">
+const title: Ref<string> = ref("")
+const content: Ref<string> = ref("")
+
+onMounted(() => {})
+
+const onSubmit = () => {
+    console.log(title.value, content.value)
+}
+</script>
