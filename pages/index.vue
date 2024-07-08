7a01a8a339fd1dc8db0f1e352a22ca0be20d899e
soundraven <kin9709@gmail.com>
2024-07-08 20:26:07 +0900

    루리웹 레이아웃 참고하여 페이지 작성중

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,41 @@
 <template>
-    <div class="w-[1024px] min-h-[800px] mx-auto border-amber-300">
-        <el-container>
-            <el-container></el-container>
-            <el-aside class="w-[200px] h-[800px] border-2 flex justify-evenly">
-                <el-button
-                    v-for="navBtn in navBtnArray"
-                    class="w-16 mx-4 flex items-center justify-center"
-                    @click="navigateTo(`/${navBtn}`)"
+    <el-container
+        class="w-[1280px] mx-auto flex flex-col flex-wrap items-center"
+    >
+        <el-container class="w-full h-full">
+            <img
+                src="https://img.ruliweb.com/view/2024/06/28/linm19480_1150x250_2.jpg"
+                alt=""
+                class="w-[1280px] h-[250px] object-cover"
+            />
+        </el-container>
+        <el-container class="flex w-full">
+            <el-container class="w-3/4 h-[550px] border-2 border-blue-400">
+                content
+            </el-container>
+            <el-aside
+                class="w-1/4 h-[550px] border-2 border-red-400 flex flex-col items-center"
+            >
+                <el-button type="primary" class="w-[240px] h-[30px] mt-2"
+                    >로그인</el-button
                 >
-                    {{ navBtn }}
-                </el-button>
+                <el-button class="w-[240px] h-[30px] mx-auto mt-2"
+                    >회원가입</el-button
+                >
+                <el-container
+                    class="flex content-start justify-evenly flex-wrap"
+                >
+                    <el-button
+                        v-for="navBtn in navBtnArray"
+                        class="w-1/4 mx-auto mt-2 flex items-center justify-center"
+                        @click="navigateTo(`/${navBtn}`)"
+                    >
+                        {{ navBtn }}
+                    </el-button></el-container
+                >
             </el-aside>
         </el-container>
-    </div>
+    </el-container>
 </template>
 
 <script setup lang="ts">
